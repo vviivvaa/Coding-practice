ab34967a7d10cc749721c225bcdaf0b2450dd25b
Vanessa <Vanessa@DESKTOP-4V4KPCV>
2020-08-02 11:06:43 +0300

    Name_Winner_ASCII_Values compares strings by the sum of their char values according to the ASCII table and prints the string with the highest value as the winner

---
+++ Coding Practice/Combinations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int lastNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            bool isFound = false;
+            int combinations = 0;
+
+            for (int i = firstNumber; i <= lastNumber; i++)
+            {
+                for (int j = firstNumber; j <= lastNumber; j++)
+                {
+                    combinations++;
+
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinations} " +
+                            $"({i} + {j} = {magicNumber})");
+
+                        isFound = true;
+
+                        break;
+                    }
+                }
+
+                if (isFound)
+                {
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ Coding Practice/Name_Winner_ASCII_Values/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Name_Winner_ASCII_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string winner = string.Empty;
+            int maxSum = 0;
+            string input = Console.ReadLine();
+
+            while (!input.Equals("STOP", StringComparison.CurrentCultureIgnoreCase))
+            {
+                int currentSum = 0;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    currentSum += input[i];
+                }
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    winner = input;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Winner is {winner} - {maxSum}!");
+        }
+    }
+}
